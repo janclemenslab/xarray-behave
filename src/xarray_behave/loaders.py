fix silent bug

src/xarray_behave/loaders.py
@@ -94,7 +94,7 @@ def load_manual_annotation(filepath):
 
     manual_events_seconds = dict()
     for key, val in mat_data.items():
-        if len(val) and val.ndim == 2 and not key.startswith('_'):  # ignore matfile metadata
+        if len(val) and hasattr(val, 'ndim') and val.ndim == 2 and not key.startswith('_'):  # ignore matfile metadata
             manual_events_seconds[key.lower() + '_manual'] = np.sort(val[:, 1:])
     return manual_events_seconds
 