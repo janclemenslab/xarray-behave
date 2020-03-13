393b18c566ae74c9ba5961083f81b282a6e4faaf
Jan Clemens <clemensjan@googlemail.com>
2020-03-13 13:00:10 +0100

    install form data and version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='xarray_behave',
-      version='0.2',
+      version='0.3',
       description='xarray_behave',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -17,6 +17,7 @@ setup(name='xarray_behave',
       license='MIT',
       packages=find_packages('src'),
       package_dir={'': 'src'},
+      package_data = {'xarray_behave': ['gui/forms/*']},
       install_requires=['PySide2', 'numpy', 'scipy', 'xarray', 'h5py', 'zarr', 'flammkuchen', 'pysoundfile'],
       extras_require={'gui': ['pyside2', 'pyqtgraph', 'simpleaudio', 'scikit-image', 'opencv-python']},
       include_package_data=True,
