c401f728f6d2ff4a2ac9f53264d4a4a2d8170565
Jan Clemens <clemensjan@googlemail.com>
2020-03-19 10:15:52 +0100

    auto version

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
 from setuptools import setup, find_packages
-import os
+import codecs
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 # read the contents of your README file
 this_directory = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +29,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='xarray_behave',
-      version='0.3',
+      version=find_version("src/xarray_behave/__init__.py"),
       description='xarray_behave',
       long_description=long_description,
       long_description_content_type="text/markdown",
--- src/xarray_behave/__init__.py
+++ src/xarray_behave/__init__.py
@@ -1,2 +1,4 @@
+__version__ = "0.4.0"
+
 from .ui import main as ui
 from .xarray_behave import assemble, assemble_metrics, load, save, from_wav
