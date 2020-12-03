8b62daa78395fd5d8545e071841005c9a509cc37
Jan Clemens <clemensjan@googlemail.com>
2020-12-03 19:31:57 +0100

    clean up dialog

--- src/xarray_behave/gui/app.py
+++ src/xarray_behave/gui/app.py
@@ -347,9 +347,8 @@ class MainWindow(pg.QtGui.QMainWindow):
 
             form_data['model_name'] = 'tcn'
             if form_data['frontend'] == 'Yes':
-                form_data['model_name'] += form_data['frontend_type']
-                form_data['pre_nb_conv'] = int(np.ceil(np.log2(form_data['pre_nb_conv'])))
-            # del form_data['frontend']
+                form_data['model_name'] += '_stft'
+                form_data['pre_nb_conv'] = int(form_data['pre_nb_conv'])
 
             form_data['reduce_lr'] = form_data['reduce_lr_patience'] is not None
             form_data = {k: v for k, v in form_data.items() if v not in ['Yes', 'No', None]}
