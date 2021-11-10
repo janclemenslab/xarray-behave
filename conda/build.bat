88d010ad3b36207ac106bb62b62cfad372f9505b
Jan Clemens <clemensjan@googlemail.com>
2021-11-10 14:44:29 +0100

    build files

--- conda/build.bat
+++ conda/build.bat
@@ -1,5 +1,3 @@
-see `.github/workflows/publish.yaml`
-
 conda mambabuild xarray-behave-nogui -c conda-forge -c ncb -c anaconda --python 3.7 --user ncb
 conda mambabuild xarray-behave -c conda-forge -c ncb -c anaconda --python 3.7 --user ncb
 conda mambabuild xarray-behave-nogui -c conda-forge -c ncb -c anaconda --python 3.8 --user ncb
