7d2d62b8414852c5d0058c06b8dfe4b7bee038e0
Jan Clemens <clemensjan@googlemail.com>
2022-02-28 09:02:59 +0100

    test imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,13 +1,14 @@
 import pytest
 
-import numpy as np
-import xarray as xr
-import pandas as pd
-
 
 def test_imports():
     import xarray_behave as xb
-    from xarray_behave import event_utils
+    import xarray_behave.event_utils
+    import xarray_behave.metrics
+    import xarray_behave.loaders
     import xarray_behave.annot
-    import xarray_behave.gui
-    import xarray_behave.io
+
+    from xarray_behave.io import (annotations, annotations_manual, audio,
+                                  balltracks, movieparams, poses, tracks)
+    from xarray_behave.gui import (app, colormaps, das, formbuilder, table,
+                                   utils, views)
