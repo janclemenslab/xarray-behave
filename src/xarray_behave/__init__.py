3bf3ac07486accf1a84b62579add60730f414b80
Jan Clemens <clemensjan@googlemail.com>
2022-05-05 22:39:24 +0200

    enfore pyside2

--- src/xarray_behave/__init__.py
+++ src/xarray_behave/__init__.py
@@ -1,4 +1,7 @@
 """xarray tools for behavioral data."""
-__version__ = "0.32.1"
+__version__ = "0.32.2"
+
 
 from .xarray_behave import assemble, assemble_metrics, load, save
+import os
+os.environ['QT_API'] = 'pyside2'
