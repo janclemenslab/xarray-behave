b2c43cbf8623a719eb36cbfec4d9125f3bb7140e
Jan Clemens <clemensjan@googlemail.com>
2022-07-12 14:44:56 +0200

    fixed cmd export

--- src/xarray_behave/gui/app.py
+++ src/xarray_behave/gui/app.py
@@ -454,6 +454,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
             if not len(form_data['y_suffix']):
                 del form_data['y_suffix']
+            if not len(form_data['save_prefix']):
+                del form_data['save_prefix']
 
             for field in ['seed', 'fraction_data', 'reduce_lr_patience']:
                 if field in form_data and form_data[field] is None:
@@ -461,7 +463,7 @@ class MainWindow(QtWidgets.QMainWindow):
             form_data['use_separable'] = [item.lower()=='true' for item in form_data['use_separable']]
             if is_cli:
                 form_data['use_separable'] = ' '.join([str(item) for item in form_data['use_separable']])
-                for field in ['ignore_boundaries', 'reduce_lr', 'tensorboard', 'post_opt']:
+                for field in ['balance', 'ignore_boundaries', 'reduce_lr', 'tensorboard', 'post_opt']:
                     if field in form_data:
                         if form_data[field] is False:  # rename and pre-prend "no_"
                             del form_data[field]
