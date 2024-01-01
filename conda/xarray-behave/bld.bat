f3b0c4b31e19fb9c513b6a562947e4783a5b8a8f
Jan Clemens <clemensjan@googlemail.com>
2024-01-01 17:07:05 +0100

    install simpleaudio

--- conda/xarray-behave/bld.bat
+++ conda/xarray-behave/bld.bat
@@ -4,8 +4,7 @@ set PIP_NO_INDEX=False
 set PIP_NO_DEPENDENCIES=False
 set PIP_IGNORE_INSTALLED=False
 
-@REM "%PYTHON%" -m pip install pysoundfile -vv
-@REM "%PYTHON%" -m pip install pyvideoreader -vv --no-dependencies
+"%PYTHON%" -m pip install simpleaudio -vv --no-dependencies
 "%PYTHON%" -m pip install pkgutil_resolve_name -vv --no-dependencies
 "%PYTHON%" -m pip install xarray-behave[gui] -vv --no-dependencies
 if errorlevel 1 exit 1
